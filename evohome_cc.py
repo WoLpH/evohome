3fb5c2330d77495d09b53c90137559f0fec97ec5
David Bonnes <david@bonnes.me>
2019-03-02 10:41:27 +0000

    add refresh_token functionality 5

--- evohome_cc.py
+++ evohome_cc.py
@@ -96,7 +96,7 @@ CONFIG_SCHEMA = vol.Schema({
 
         vol.Optional(CONF_REFRESH_TOKEN, default=''): cv.string,
         vol.Optional(CONF_ACCESS_TOKEN, default=''): cv.string,
-        vol.Optional(CONF_ACCESS_TOKEN_EXPIRES, default=''): cv.datetime,
+        vol.Optional(CONF_ACCESS_TOKEN_EXPIRES, default=''): cv.string,
     }),
 }, extra=vol.ALLOW_EXTRA)
 
@@ -199,9 +199,18 @@ def setup(hass, hass_config):
     from evohomeclient2 import EvohomeClient
     _LOGGER.warn("setup(): API call [4 request(s)]: client.__init__()...")       # noqa: E501; pylint: disable=line-too-long; ZXDEL
 
-    _LOGGER.warn("refresh_token: %s", evo_data['params'][CONF_REFRESH_TOKEN])                # TODO: debug code, delete me
-    _LOGGER.warn("access_token: %s", evo_data['params'][CONF_ACCESS_TOKEN])                  # TODO: debug code, delete me
-    _LOGGER.warn("access_token_expires: %s", evo_data['params'][CONF_ACCESS_TOKEN_EXPIRES])  # TODO: debug code, delete me
+    refresh_token = evo_data['params'][CONF_REFRESH_TOKEN])
+    access_token = evo_data['params'][CONF_ACCESS_TOKEN])
+    access_token_expires = evo_data['params'][CONF_ACCESS_TOKEN_EXPIRES]
+    
+    if not refresh_token:
+        refresh_token = None
+    if not access_token or not access_token_expires:
+        access_token = access_token_expires = None
+    
+    _LOGGER.warn("refresh_token: %s", refresh_token)                             # TODO: debug code, delete me
+    _LOGGER.warn("access_token: %s", access_token)                               # TODO: debug code, delete me
+    _LOGGER.warn("access_token_expires: %s", access_token_expires)               # TODO: debug code, delete me
 
     try:
         client = evo_data['client'] = EvohomeClient(
